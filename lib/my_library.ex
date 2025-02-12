b9f9cad9a55aa85eca69e5380eb2ba335b1d9a76
Marcelo Dominguez <952074+marpo60@users.noreply.github.com>
2025-02-12 13:29:59 -0300

    feat: Add feature using plug_crypto

--- lib/my_library.ex
+++ lib/my_library.ex
@@ -12,7 +12,20 @@ defmodule MyLibrary do
       :world
 
   """
+
+  require Logger
+
   def hello do
     :world
+  end
+
+  def encrypt_hello do
+    Logger.info("Plug Crypto version: #{Application.spec(:plug_crypto, :vsn)}")
+
+    Plug.Crypto.encrypt("key_base", "secret", hello())
+  end
+
+  def decrypt_hello(token) do
+    Plug.Crypto.decrypt("key_base", "secret", token)
   end
 end
